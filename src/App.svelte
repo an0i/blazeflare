cae97324948ba805e43c211f51422634a222680d
an0i <45888210+an0i@users.noreply.github.com>
2024-01-04 21:44:03 +0800

    Improve setting part

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,25 @@
 <script>
+  import avatar from "./avatar.webp";
   import { SvgFile, SvgFire, SvgFolder, SvgLogin, SvgLogout, SvgSetting } from "./components/icons";
-  import { HashRoute, formatBytes } from "./utils";
-  if (location.hash === "") location.hash = "#list#";
-  const routeNow = new HashRoute(decodeURIComponent(location.hash));
+  import { Hash, formatBytes } from "./utils";
 
-  const logined = document.cookie.includes("id_token");
-  const nickname = logined ? JSON.parse(localStorage.getItem("user_info")).nickname : "everyone";
+  const defaultHash = "#list#";
+  if (window.location.hash === "") window.location.hash = defaultHash;
+  const currentHash = new Hash(decodeURIComponent(location.hash));
+
+  const userInfo = JSON.parse(window.localStorage.getItem("user_info")) || { nickname: "everyone", picture: avatar };
+  const isLogined = document.cookie.includes("id_token") && !(userInfo === null);
+
+  let showBzEmpty = localStorage.getItem("hide_bzempty") === "yes" || false;
+  $: {
+    localStorage.setItem("hide_bzempty", showBzEmpty ? "yes" : "no");
+  }
 </script>
 
 <svelte:window
   on:hashchange={(e) => {
-    if (new URL(e.newURL).hash === "") location.hash = "#list#";
-    routeNow.hash = decodeURIComponent(new URL(e.newURL).hash);
+    if (new URL(e.newURL).hash === "") location.hash = defaultHash;
+    currentHash.value = decodeURIComponent(new URL(e.newURL).hash);
   }}
 />
 
@@ -22,46 +30,43 @@
       <a href={import.meta.env.BASE_URL} class="ifnocss">BlazeFlare</a>
     </h1>
 
-    <ul id="navigator" class="ml-auto box-border flex gap-1.5 border-l p-1.5">
+    <ul id="navigator" class="ml-auto box-border flex gap-1.5 p-1.5">
       <li>
-        <a href="{import.meta.env.BASE_URL}#list#" class="block size-11 rounded-full p-1.5 {routeNow.getAction() === '#list#' ? 'border border-yellow-500 fill-yellow-500' : 'fill-gray-300'} hover:bg-gray-100 active:hover:bg-gray-200"><SvgFolder class="size-full" /></a>
+        <a href="{import.meta.env.BASE_URL}#list#" class="block size-11 rounded-full p-1.5 {currentHash.getAction() === '#list#' ? 'border border-yellow-500 fill-yellow-500' : 'fill-gray-300'} hover:bg-gray-100 active:hover:bg-gray-200"><SvgFolder class="size-full" /></a>
         <a href="{import.meta.env.BASE_URL}#list#" class="ifnocss">文件</a>
       </li>
-      <li>
-        <a href="{import.meta.env.BASE_URL}#setting#" class="block size-11 rounded-full p-1.5 {routeNow.getAction() === '#setting#' ? 'border border-blue-500 fill-blue-500' : 'fill-gray-300'} hover:bg-gray-100 active:hover:bg-gray-200"><SvgSetting class="size-full" /></a>
-        <a href="{import.meta.env.BASE_URL}#setting#" class="ifnocss">设置</a>
-      </li>
-    </ul>
-
-    <div>
-      {#if logined}
-        <a href="{import.meta.env.BASE_URL}user/logout.html" class="box-border block size-14 border-l fill-gray-300 p-3 hover:bg-gray-100 active:hover:bg-gray-200"><SvgLogout class="size-full" /></a>
-        <a href="{import.meta.env.BASE_URL}user/logout.html" class="ifnocss">登出</a>
+      {#if isLogined}
+        <li>
+          <a href="{import.meta.env.BASE_URL}#setting#" class="block size-11 rounded-full {currentHash.getAction() === '#setting#' ? 'border border-blue-500 fill-blue-500' : 'fill-gray-300'} hover:bg-gray-100 active:hover:bg-gray-200"><img src={userInfo.picture} class="size-full" alt="user avatar" /></a>
+          <a href="{import.meta.env.BASE_URL}#setting#" class="ifnocss">设置</a>
+        </li>
       {:else}
-        <a href="{import.meta.env.BASE_URL}user/login.html" class="box-border block size-14 border-l fill-gray-300 p-3 hover:bg-gray-100 active:hover:bg-gray-200"><SvgLogin class="size-full" /></a>
-        <a href="{import.meta.env.BASE_URL}user/login.html" class="ifnocss">登录</a>
+        <li>
+          <a href="{import.meta.env.BASE_URL}#setting#" class="block size-11 rounded-full p-1.5 {currentHash.getAction() === '#setting#' ? 'border border-blue-500 fill-blue-500' : 'fill-gray-300'} hover:bg-gray-100 active:hover:bg-gray-200"><SvgSetting class="size-full" /></a>
+          <a href="{import.meta.env.BASE_URL}#setting#" class="ifnocss">设置</a>
+        </li>
       {/if}
-    </div>
+    </ul>
   </div>
 
   <div id="action" class="flex grow flex-col">
-    {#if routeNow.getAction() === "#list#"}
+    {#if currentHash.getAction() === "#list#"}
       <div id="list" class="flex grow flex-col">
         <p class="ifnocss">位置</p>
         <ul id="list-breadcrumb" class="box-border flex flex-wrap items-center gap-1 border-b p-2 text-gray-500">
           <li>
-            {#if routeNow.getPrefixInfixSuffix() !== ""}
-              <a href="{import.meta.env.BASE_URL}{routeNow.getAction()}" class="block rounded-full bg-gray-50 px-2 py-1 hover:bg-gray-100 active:hover:bg-gray-200">根目录</a>
+            {#if currentHash.getPrefixInfixSuffix() !== ""}
+              <a href="{import.meta.env.BASE_URL}{currentHash.getAction()}" class="block rounded-full bg-gray-50 px-2 py-1 hover:bg-gray-100 active:hover:bg-gray-200">根目录</a>
             {:else}
               <span class="block rounded-full px-2 py-1 text-black">根目录</span>
             {/if}
           </li>
-          {#each routeNow.getSegments() as segment, i}
+          {#each currentHash.getSegments() as segment, i}
             <span class="before:content-['/']"></span>
-            {#if i !== routeNow.getSegments().length - 1}
+            {#if i !== currentHash.getSegments().length - 1}
               <li class="max-w-full">
                 <a
-                  href="{import.meta.env.BASE_URL}{routeNow.getAction()}{routeNow
+                  href="{import.meta.env.BASE_URL}{currentHash.getAction()}{currentHash
                     .getSegments()
                     .slice(0, i + 1)
                     .join('/') + '/'}"
@@ -73,28 +78,28 @@
             {/if}
           {/each}
         </ul>
-        {#await fetch(`/api/getTree/${nickname}`).then((res) => res.json())}
+        {#await fetch(`/api/getTree/${userInfo.nickname}`).then((res) => res.json())}
           <div id="list-loading" class="grow">
             <p class="p-2">正在加载文件树...</p>
           </div>
-        {:then root}
+        {:then tree}
           {#await new Promise((resolve, reject) => {
-            let current = root;
-            const segments = routeNow.getSegments();
+            let current = tree;
+            const segments = currentHash.getSegments();
             for (let i = 0; i < segments.length; ++i) {
               const segment = segments[i];
-              let findResult = current.nodes.find((node) => (i !== segments.length - 1 ? node.isFolder : node.isFolder === routeNow.isFolder()) && node.name === segment);
-              if (findResult === undefined) return reject(new Error("文件树中找不到该文件(夹)"));
-              current = findResult;
+              let find = current.nodes.find((node) => (i !== segments.length - 1 ? node.isFolder : node.isFolder === currentHash.isFolder()) && node.name === segment);
+              if (find === undefined) return reject(new Error("文件树中找不到该文件(夹)"));
+              current = find;
             }
             return resolve(current);
           }) then currentNode}
             <p class="ifnocss">文件</p>
-            {#if routeNow.isFolder()}
+            {#if currentHash.isFolder()}
               <ul id="list-folderview" class="h-0 grow overflow-y-auto overscroll-y-none">
-                {#each currentNode.nodes as node}
+                {#each currentNode.nodes.filter((node) => (showBzEmpty ? true : node.name !== ".bzEmpty")) as node}
                   <li>
-                    <a class="box-border flex gap-2 border-b p-2 hover:bg-gray-100 active:hover:bg-gray-200" href="{import.meta.env.BASE_URL}{routeNow.hash}{node.name}{node.isFolder ? '/' : ''}">
+                    <a class="box-border flex gap-2 border-b p-2 hover:bg-gray-100 active:hover:bg-gray-200" href="{import.meta.env.BASE_URL}{currentHash.value}{node.name}{node.isFolder ? '/' : ''}">
                       {#if node.isFolder}
                         <SvgFolder class="size-11 fill-blue-300" />
                       {:else}
@@ -119,7 +124,7 @@
               <div id="list-fileview" class="grow">
                 <h3 class="m-2">
                   <SvgFile class="inline size-8 fill-blue-300" />
-                  <a class="break-all align-middle text-lg font-semibold text-sky-500 underline hover:text-sky-600 hover:decoration-2" href="/api/getFile/{routeNow.getSegments().join('/')}">{currentNode.name}</a>
+                  <a class="break-all align-middle text-lg font-semibold text-sky-500 underline hover:text-sky-600 hover:decoration-2" href="/api/getFile/{currentHash.getSegments().join('/')}">{currentNode.name}</a>
                 </h3>
                 <span class="m-2 block text-sm">
                   <p class="inline-block max-w-full truncate rounded-full border px-2 py-1 before:content-['字节:_']"><span class="ifnocss">Size: </span>{currentNode.size}</p>
@@ -142,17 +147,35 @@
           </div>
         {/await}
       </div>
-    {:else if routeNow.getAction() === "#setting#"}
+    {:else if currentHash.getAction() === "#setting#"}
       <div id="setting" class="grow">
         <div class="box-border border-b p-2">
           <span class="block px-2 py-1">设置</span>
         </div>
-        <div class="p-2">WIP</div>
+
+        <div class="flex h-14 items-center truncate border-b">
+          <img src={userInfo.picture} alt="user avatar" class="ml-2 box-border inline-block h-12 rounded-full border" />
+          <span class="ml-2 inline-block text-lg">{isLogined ? userInfo.nickname : "请登入"}</span>
+          {#if isLogined}
+            <a href="{import.meta.env.BASE_URL}user/logout.html" class="ml-auto box-border block h-full border-l fill-gray-300 p-3 hover:bg-gray-100 active:hover:bg-gray-200"><SvgLogout class="size-full" /></a>
+            <a href="{import.meta.env.BASE_URL}user/logout.html" class="ifnocss">登出</a>
+          {:else}
+            <a href="{import.meta.env.BASE_URL}user/login.html" class="ml-auto box-border block h-full shrink-0 border-l fill-gray-300 p-3 hover:bg-gray-100 active:hover:bg-gray-200"><SvgLogin class="size-full" /></a>
+            <a href="{import.meta.env.BASE_URL}user/login.html" class="ifnocss">登入</a>
+          {/if}
+        </div>
+        <div class="flex h-14 items-center border-b p-4">
+          显示 .bzEmpty
+          <label class="relative ml-auto inline-flex cursor-pointer items-center">
+            <input type="checkbox" bind:checked={showBzEmpty} class="peer sr-only" />
+            <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-500 peer-checked:after:translate-x-full peer-focus:outline-none"></div>
+          </label>
+        </div>
       </div>
     {:else}
       <div id="error" class="grow">
         <div class="p-2">
-          <p>未知的操作: {routeNow.getAction()}</p>
+          <p>未知的操作: {currentHash.getAction()}</p>
           <p>发生什么事了，你怎么跑到这来了？</p>
         </div>
       </div>
--- src/utils.js
+++ src/utils.js
@@ -1,36 +1,33 @@
 // a?pis (#a#)(pp/)(ii/)(sss)
-export function HashRoute(/**@type {string}*/ hash) {
-  this.hash = hash;
+export class Hash {
+  constructor(/**@type {string}*/ value) {
+    this.value = value;
+  }
+  getSegments() {
+    if (this.getPrefixInfixSuffix() === "") return [];
+    return this.isFolder() ? this.getPrefixInfixSuffix().slice(0, -1).split("/") : this.getPrefixInfixSuffix().split("/");
+  }
+  isFolder() {
+    return this.value.endsWith("/") || this.getPrefixInfixSuffix() === "";
+  }
+  getInfix(/**@type {Hash}*/ routeAsPrefix) {
+    const is = this.getPrefixInfixSuffix().slice(routeAsPrefix.getPrefixInfixSuffix().length);
+    const firstSlashIndex = is.indexOf("/");
+    const i = firstSlashIndex === -1 ? is : is.slice(0, firstSlashIndex + 1);
+    return i;
+  }
+  getPrefixInfixSuffix() {
+    return this.value.slice(this.value.indexOf("#", 1) + 1);
+  }
+  getAction() {
+    return this.value.slice(0, this.value.indexOf("#", 1) + 1);
+  }
 }
 
-HashRoute.prototype.getSegments = function () {
-  if (this.getPrefixInfixSuffix() === "") return [];
-  return this.isFolder() ? this.getPrefixInfixSuffix().slice(0, -1).split("/") : this.getPrefixInfixSuffix().split("/");
-};
-
-HashRoute.prototype.isFolder = function () {
-  return this.hash.endsWith("/") || this.getPrefixInfixSuffix() === "";
-};
-
-HashRoute.prototype.getInfix = function (/**@type {HashRoute}*/ routeAsPrefix) {
-  const is = this.getPrefixInfixSuffix().slice(routeAsPrefix.getPrefixInfixSuffix().length);
-  const firstSlashIndex = is.indexOf("/");
-  const i = firstSlashIndex === -1 ? is : is.slice(0, firstSlashIndex + 1);
-  return i;
-};
-
-HashRoute.prototype.getPrefixInfixSuffix = function () {
-  return this.hash.slice(this.hash.indexOf("#", 1) + 1);
-};
-
-HashRoute.prototype.getAction = function () {
-  return this.hash.slice(0, this.hash.indexOf("#", 1) + 1);
-};
-
-export function formatBytes(/** @type number */ bytes, /** @type number */ decimals) {
+export function formatBytes(/** @type number */ bytes, /** @type number */ decimals = 2) {
   if (bytes === 0) return "0 Bytes";
   const k = 1024;
-  const dm = decimals || 2;
+  const dm = decimals;
   const sizes = ["Bytes", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
   return `${parseFloat((bytes / k ** i).toFixed(dm))} ${sizes[i]}`;
